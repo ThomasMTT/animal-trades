696d2da30814f59a5d45969986217adf37d50688
ThomasMTT <thomasmatthys@protonmail.com>
2024-09-25 16:16:58 +0200

    Add: Version 1.0

---
+++ src/main/java/life/thoms/mods/animal_trades/event/ModPlayerEvents.java
@@ -0,0 +1,101 @@
+package life.thoms.mods.animal_trades.event;
+
+import life.thoms.mods.animal_trades.AnimalTrades;
+import net.minecraft.core.Holder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.InteractionHand;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import static life.thoms.mods.animal_trades.AnimalTrades.MOD_ENABLED;
+import static life.thoms.mods.animal_trades.util.EffectUtils.sendSoundAndParticles;
+import static life.thoms.mods.animal_trades.util.TradeUtils.getAvailableTrade;
+
+/**
+ * Event handler class for the Animal Trades mod.
+ * This class listens for player interactions with entities to facilitate trading.
+ */
+@Mod.EventBusSubscriber(modid = AnimalTrades.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class ModPlayerEvents {
+
+    // Store cooldowns in memory
+    private static final Map<UUID, Long> cooldowns = new HashMap<>();
+
+    /**
+     * Handles the event when a player right-clicks on an entity.
+     * Facilitates the trading process with the specified animal.
+     *
+     * @param event The event containing interaction data between the player and the entity.
+     */
+    @SubscribeEvent
+    public static void onEntityRightClick(PlayerInteractEvent.EntityInteractSpecific event) {
+        if (!MOD_ENABLED) {
+            return;
+        }
+
+        // Get hand
+        InteractionHand hand = event.getHand();
+
+        // Only allow the event to proceed for the main hand
+        if (hand != InteractionHand.MAIN_HAND) {
+            return;
+        }
+
+        Player player = event.getEntity();
+
+        // Check if the target is a living entity (animal)
+        if (!(event.getTarget() instanceof LivingEntity animal)) {
+            return;
+        }
+
+        List<String> trade = getAvailableTrade(player, animal, hand);
+
+        // If trade is available
+        if (trade == null || trade.size() < 5) {
+            return;
+        }
+
+        // Initialize variables for trade processing
+        Item animalCurrency = ForgeRegistries.ITEMS.getValue(new ResourceLocation(trade.get(1)));
+        int price;
+        MobEffect playerEffect;
+        int effectDuration;
+
+        // Validate and parse values
+        try {
+            price = Integer.parseInt(trade.get(2));
+            playerEffect = ForgeRegistries.MOB_EFFECTS.getValue(new ResourceLocation(trade.get(3)));
+            effectDuration = Integer.parseInt(trade.get(4));
+        } catch (NumberFormatException ignored) {
+            return;
+        }
+
+        // Validate values and process trade
+        if (animalCurrency != null && price > 0 && playerEffect != null && effectDuration > 0) {
+            // Remove currency from player inventory if not creative
+            if (!player.isCreative()) {
+                player.getItemInHand(hand).shrink(price);
+            }
+
+            // Remove existing effect before applying the new one
+            player.removeEffect(playerEffect);
+            player.addEffect(new MobEffectInstance(playerEffect, effectDuration, 0));
+            sendSoundAndParticles(player, animal, SoundEvents.NOTE_BLOCK_CHIME.get(), ParticleTypes.HEART);
+
+        }
+    }
+}
---
+++ src/main/java/life/thoms/mods/animal_trades/util/TradeUtils.java
@@ -0,0 +1,99 @@
+package life.thoms.mods.animal_trades.util;
+
+import net.minecraft.core.Holder;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static life.thoms.mods.animal_trades.config.ModConfig.*;
+import static life.thoms.mods.animal_trades.util.EffectUtils.sendSoundAndParticles;
+
+/**
+ * Utility class for handling trade operations in the Animal Trades mod.
+ */
+public class TradeUtils {
+
+    /**
+     * Retrieves available trades for a given player and animal.
+     * <p>
+     * This method checks the trades list for valid trades based on the player's inventory,
+     * the animal involved, and any existing effects on the player. If a valid trade is found,
+     * it returns the details of the trade; otherwise, it returns null.
+     *
+     * @param player The player attempting to trade.
+     * @param animal The animal involved in the trade.
+     * @param hand   The hand used for trading
+     * @return A list of trade details if a valid trade is found; null otherwise.
+     */
+    public static List<String> getAvailableTrade(Player player, Entity animal, InteractionHand hand) {
+
+        if (tradesList.isEmpty()) {
+            if (!tradeStrList.get().isEmpty()) {
+                parseTrades();
+            }
+        }
+
+        // Check for all possible trades
+        for (List<String> innerListOriginal : tradesList) {
+
+            // Create a copy to avoid overwriting the tradesList
+            List<String> innerList = new ArrayList<>(innerListOriginal);
+
+            // Compare names
+            if (!innerList.get(0).equals(animal.getEncodeId())) {
+                continue;
+            }
+
+            // Get existing effect of this type if exists
+            MobEffect playerEffect = ForgeRegistries.MOB_EFFECTS
+                    .getValue(new ResourceLocation(innerList.get(3)));
+
+            // If it doesn't exist check next trade
+            if (playerEffect == null) {
+                continue;
+            }
+
+            // Initialize effect variables
+            MobEffectInstance existingEffect = player.getEffect(playerEffect);
+            int effectDuration = Integer.parseInt(innerList.get(4));
+            int maxEffectDuration = Integer.parseInt(innerList.get(5));
+
+            // Compare items in main hands
+            if (!innerList.get(1).equals(player.getItemInHand(hand).getDescriptionId()
+                    .replace("item.", "")
+                    .replace(".", ":"))) {
+                continue;
+            }
+
+            // Check if effect duration is longer than allowed
+            if (existingEffect != null) {
+                effectDuration += existingEffect.getDuration();
+                if (effectDuration > maxEffectDuration) {
+                    sendSoundAndParticles(player, animal, SoundEvents.NOTE_BLOCK_BASS.get(), ParticleTypes.CLOUD);
+                    continue;
+                }
+                innerList.set(4, String.valueOf(effectDuration));
+            }
+
+            // Check amount >= price
+            int price = Integer.parseInt(innerList.get(2));
+            if (player.getItemInHand(hand).getCount() >= price && price > 0) {
+                return innerList; // Return trade details
+            } else {
+                sendSoundAndParticles(player, animal, SoundEvents.NOTE_BLOCK_BASS.get(), ParticleTypes.CLOUD);
+            }
+        }
+
+        // Returns null if nothing is found
+        return null;
+    }
+}
